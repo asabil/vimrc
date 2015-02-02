2f08c5f2d50841bbba0dbaf01b4ad8652344c214
Ali Sabil <ali.sabil@soundrop.com>
2015-02-02 11:09:54 +0100

    Replace the Makefile with an install.sh and restructure

--- Makefile
+++
@@ -1,7 +0,0 @@
-install:
-	git submodule init
-	git submodule update
-	rm -f ~/.vimrc
-	ln -s `pwd`/vimrc ~/.vimrc
-
-.PHONY: install
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$(readlink "$SOURCE")"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+link() {
+	local name=$1;
+	[[ -z "$name" ]] && echo "Expecting name" && exit 1;
+	rm -rf ~/."${name}"
+	ln -s "${DIR}/${name}" ~/."${name}"
+}
+
+link "vim"
+link "vimrc"
